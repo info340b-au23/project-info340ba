297536be71e8b52c2b0884cb4f3dc07f671fe11d
simku22 <simku22@uw.edu>
2023-11-13 15:13:12 -0800

    added more routing functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HomePage } from './HomePage.js';
-import { Footer } from './Footer.js'
+import { Footer } from './Footer.js';
+import { NavBar } from './NavBar.js';
+import { Banner } from './Banner.js';
 
 export default function App() {
     return (
         <BrowserRouter>
+            <NavBar />
             <Routes>
+                <Route path="index" element={<HomePage />}/>
                 <Route path="/" element={<HomePage />}/>
+                <Route path='about' element={<HomePage />}/>
             </Routes>
+            <Banner />
             <Footer />
         </BrowserRouter>
     )
