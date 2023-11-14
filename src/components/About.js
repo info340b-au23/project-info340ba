2d0781e18d648ed4bd710856fb0c348b2d50ac17
simku22 <simku22@uw.edu>
2023-11-13 20:42:46 -0800

    added about us page and and routed it in

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export function About() {
+    return (
+        <div className="container justify-content-center text-center p-5">
+            <section className="header-center">
+                <h2>ABOUT US!</h2>
+                <p>Our team is comprised of four undergraduate students currently studying Informatics. </p>
+            </section>
+            <div className="container p-5">
+                <div className="row drawings d-flex text-center">
+                    <div className="col-sm-6 col-md-3 col-lg-3">
+                        <img src="img/simon.JPG" className="drawings" alt="simons drawing of themself" />
+                    </div>
+                    <div className="col-sm-6 col-md-3 col-lg-3">
+                        <img src="img/marvin.JPG" className="drawings" alt="marvins drawing of themself" />
+                    </div>
+                    <div className="col-sm-6 col-md-3 col-lg-3">
+                        <img src="img/eddie.JPG" className="drawings" alt="eddie's drawing of themself" />
+                    </div>
+                    <div className="col-sm-6 col-md-3 col-lg-3">
+                        <img src="img/jesse.JPG" className="drawings" alt="jesse's drawing of themself" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
