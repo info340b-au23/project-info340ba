f0a9244c7bbe1f5205639f414fe17182569ed57c
simku22 <simku22@uw.edu>
2023-11-15 20:45:23 -0800

    made default page homepage

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ export default function App(props) {
         <BrowserRouter>
             <NavBar />
             <Routes>
-                <Route path="index" element={<HomePage data={props.data}/>}/>
+                <Route index element={<HomePage data={props.data}/>}/>
                 <Route path='about' element={<About />}/>
             </Routes>
             <Banner />
