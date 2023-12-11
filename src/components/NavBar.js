6f928ce4380df2211b1bc8bce1b0586a122dfbc8
Jesse Kim <jessaykym@Jesses-MacBook-Pro-4.local>
2023-12-10 17:49:26 -0800

    testing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ export function NavBar(props) {
     return (
         <div className="container">
             <nav className="navbar-border navbar navbar-expand-lg navbar-light bg-white">
-                <button className="custom-navbar-toggler navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
