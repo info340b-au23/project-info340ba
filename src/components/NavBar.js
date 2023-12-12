ab0c7c84a2f27fc6e2002ddfe5f7b88770933da9
Jesse Kim <jessaykym@Jesses-MacBook-Pro-4.local>
2023-12-11 18:53:13 -0800

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
 import '../index.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+//Responive Navbar implemented with react bootstrap
+//https://react-bootstrap.netlify.app/docs/components/navbar
 export function NavBar(props) {
     return (
-        //used react boostrap documentation base templates to help implement this design
-        //https://react-bootstrap.netlify.app/docs/components/navbar/
-            <>
-              <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                  <Navbar.Brand href="#home">PATHWISE</Navbar.Brand>
-                  <Nav className="me-auto">
-                    <Nav.Link href="index">Home</Nav.Link>
-                    <Nav.Link href="insights">Insights</Nav.Link>
-                    <Nav.Link href="classes">Classes</Nav.Link>
-                    <Nav.Link href="crowdsource">Crowdsource</Nav.Link>
-                    <Nav.Link href="about">About</Nav.Link>
-                  </Nav>
-                </Container>
-              </Navbar>
-              <br />
-            </>
+    <Navbar collapseOnSelect expand="lg" data-bs-theme="dark" className="nav-bg">
+      <Container>
+        <Navbar.Brand href="#home">PATHWISE</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="insights">Insights</Nav.Link>
+            <Nav.Link href="classes">Classes</Nav.Link>
+            <Nav.Link href="crowdsource">Crowd Source</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <a href="https://github.com/info340b-au23/project-info340ba" target="#">
+              <img id="growingIcon" className="nav-icon p-1" src="img/github.svg" alt="github company logo"/>
+            </a>
+            <a href="https://discord.com/" target="#">
+              <img id="growingIcon" className="nav-icon p-1" src="img/discord.svg" alt="discord company logo"/>
+            </a>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     )
 }   
